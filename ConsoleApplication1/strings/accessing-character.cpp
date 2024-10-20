3034e631c761a02e767cb2c6ae41818ef2838b54
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-20 22:00:45 +0500

    setuo

--- ConsoleApplication1/strings/accessing-character.cpp
+++ ConsoleApplication1/strings/accessing-character.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main3() {
 	std::cout << std::endl;
 
 	std::string str1{ "Hello there" };
---
+++ ConsoleApplication1/strings/string-size-capacity.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::cout << std::endl;
+
+	std::cout << "std::string size and capacity" << std::endl;
+
+	std::cout << std::endl;
+	return 0;
+}
