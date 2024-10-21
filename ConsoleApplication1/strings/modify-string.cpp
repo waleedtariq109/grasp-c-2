bdc3eb9b8cbe9a4b1b31b9c54244b18e93e15972
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-21 20:49:44 +0500

    std::string::insert()

--- ConsoleApplication1/strings/modify-string.cpp
+++ ConsoleApplication1/strings/modify-string.cpp
@@ -26,6 +26,36 @@ int main() {
 	std::cout << "str1: " << str1 << std::endl;
 	std::cout << "str1 size: " << str1.size() << std::endl;
 	std::cout << "str1 capacity: " << str1.capacity() << std::endl;
+	std::cout << std::endl;
+
+	/*
+		We can use std::string::insert() to insert a character
+		or string
+	*/
+
+	std::cout << "std::string::insert()" << std::endl;
+	std::string str2{ "122" };
+
+	std::cout << "str2: " << str2 << std::endl;
+
+	/*
+		@param1: Index where we want to add a character
+		@param2: How many character we want to add
+		@param3: The character which we want to add
+	*/
+	str2.insert(1, 1, '3');
+
+	std::cout << "str2 after insert: " << str2 << std::endl;
+	std::cout << std::endl;
+
+	// We can also insert c-string
+	std::string str3{ "Hello" };
+	const char* str4{ " World" };
+
+	str3.insert(5, str4);
+
+	std::cout << "str3: " << str3 << std::endl;
+
 
 	std::cout << std::endl;
 }
