c6efba731ce9e6bb39d0a2eadec09b42a45d22bc
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-23 02:41:18 +0500

    function prototype and referenc

--- ConsoleApplication1/functions/declaration-defination.cpp
+++ ConsoleApplication1/functions/declaration-defination.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
-int main() {
+static int Max(int num1, int num2); // Function Declaration or Prototype
 
+int main() {
+	std::cout << "Function Declaration and Defination" << std::endl;
+
+	int a = 20;
+	int b = 44;
+
+	int result = Max(a, b);
+	std::cout << "result: " << result << std::endl;
+
+}
+
+// Function defination or reference
+static int Max(int num1, int num2) {
+	if (num1 > num2) {
+		return num1;
+	}
+	else {
+		return num2;
+	}
 }
