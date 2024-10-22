44ac782031366962342371a614b65bc8aa40611d
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-22 23:19:22 +0500

    compare c-string

--- ConsoleApplication1/strings/comparingString.cpp
+++ ConsoleApplication1/strings/comparingString.cpp
@@ -26,5 +26,23 @@ int main() {
 	std::cout << helloStr << " > " << belloStr << " : " << (helloStr < belloStr) << std::endl;
 
 	std::cout << std::endl;
+
+	/*
+		We can also compare std::string to c-string
+	*/
+
+	const char* cString{ "Hello" };
+
+	std::cout << "helloStr.size(): " << helloStr.size() << std::endl;
+	std::cout << "std::strlen(cString): " << std::strlen(cString) << std::endl;
+	std::cout << helloStr << " == " << cString << " (c-string): " << (helloStr == cString) << std::endl;
+
+	std::cout << cString << " (c-string) " << " == " << helloStr << ": " << (cString == helloStr) << std::endl;
+	std::cout << cString << " (c-string) " << " >= " << helloStr << ": " << (cString >= helloStr) << std::endl;
+	std::cout << cString << " (c-string) " << " <= " << helloStr << ": " << (cString <= helloStr) << std::endl;
+	std::cout << cString << " (c-string) " << " < " << helloStr << ": " << (cString < helloStr) << std::endl;
+	std::cout << cString << " (c-string) " << " > " << helloStr << ": " << (cString > helloStr) << std::endl;
+
+	std::cout << std::endl;
 	return 0;
 }
