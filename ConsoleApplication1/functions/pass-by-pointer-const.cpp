3b177376e3d79fc1669fe1c93f00e5df4fe0cb24
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-24 00:41:07 +0500

    pass by ptr to const

--- ConsoleApplication1/functions/pass-by-pointer-const.cpp
+++ ConsoleApplication1/functions/pass-by-pointer-const.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
-int main() {
+static void sayAge(const int* age) {
+	//++(*age); // Compiler Error becuase it's a const pointer
 
+	/*
+		We cannot modify the value but we can make the point to point somewhere else
+		If we want to prevent this we have to make the pointer const like
+		const ptr to const int
+	*/
+
+	int age1 = 98;
+	age = &age1;
+	std::cout << "age: (function) " << *age << std::endl;
+}
+
+int main() {
+	int age{ 23 };
+	std::cout << "age: (before): " << age << std::endl; // 23
+	sayAge(&age); // pass the pointer										// 23
+	std::cout << "age: (after): " << age << std::endl;	// 23
 }
