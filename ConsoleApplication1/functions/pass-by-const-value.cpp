26b470b9a67904c37322d4d26df7beaccaa755b6
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-24 00:21:34 +0500

    pass by const value

--- ConsoleApplication1/functions/pass-by-const-value.cpp
+++ ConsoleApplication1/functions/pass-by-const-value.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+static void sayAge(const int age) {
+	//++age; // Compiler error
+	std::cout << "age: " << age << std::endl;
+}
+
 int main() {
 
+	int age{ 23 };
+
+	sayAge(age);
 }
