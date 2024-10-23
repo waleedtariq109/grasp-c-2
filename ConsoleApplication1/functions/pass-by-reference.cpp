f5dceacb0912660901582e9cedf046f705f41213
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-24 01:09:14 +0500

    pass by ref

--- ConsoleApplication1/functions/pass-by-reference.cpp
+++ ConsoleApplication1/functions/pass-by-reference.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
-int main() {
+static void Increment(int& value) {
+	++value;
+}
 
+int main() {
+	int num{ 99 };
+	std::cout << "num: " << num << std::endl;
+	Increment(num);
+	std::cout << "num: " << num << std::endl;
+	/*
+		Pass by reference which means the value param is only the alias
+		of num. So if we modify the value param it will also effect the
+		num variable
+	*/
 }
