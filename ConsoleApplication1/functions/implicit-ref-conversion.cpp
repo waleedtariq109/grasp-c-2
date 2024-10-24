3c5fa586ead020980614c23d1210705f6a507918
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-24 16:07:24 +0500

    ref imlicit conversion

--- ConsoleApplication1/functions/implicit-ref-conversion.cpp
+++ ConsoleApplication1/functions/implicit-ref-conversion.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
-int main() {
+static void printValue(const int& param) {
+	std::cout << "value: " << param << std::endl;
+}
 
+static void increment(int& param) {
+	++param;
+	std::cout << "increment: " << param << std::endl;
+}
+
+int main() {
+	double variable{ 22.44 };
+	int var1{ 33 };
+
+	printValue(variable);
+	//increment(variable); // Compiler: We cannot pass double to non const reference
+	increment(var1);
 }
