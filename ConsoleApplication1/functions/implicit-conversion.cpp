5cd74559331ba817e436369148ec79084c5d3a0c
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-24 15:52:50 +0500

    implicit conversion with ptr

--- ConsoleApplication1/functions/implicit-conversion.cpp
+++ ConsoleApplication1/functions/implicit-conversion.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
-int main() {
+static void printSum(int param1, int param2) {
+	int sum = param1 + param2;
 
+	std::cout << "sizeof(param1): " << sizeof(param1) << std::endl;
+	std::cout << "sizeof(param2): " << sizeof(param2) << std::endl;
+
+	std::cout << "sum: " << sum << std::endl;
+}
+
+int main() {
+	int a{ 22 };
+	int b{ 87 };
+
+	double x{ 93.4 };
+	double y{ 82.98 };
+
+	printSum(a,b);  // Compiler will not do implicit conversion
+	printSum(x, y); // Compiler will implicit convert into int
 }
