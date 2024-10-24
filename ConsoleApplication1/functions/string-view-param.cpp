159e1db400896127dac284bc2dcbb0585dcae1ae
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-25 02:42:55 +0500

    setup

--- ConsoleApplication1/functions/string-view-param.cpp
+++ ConsoleApplication1/functions/string-view-param.cpp
@@ -5,7 +5,7 @@ static void sayTheName(std::string_view name) {
 	std::cout << "name: " << name << std::endl;
 }
 
-int main() {
+static int stringViewParam() {
 	std::string name{ "Waleed" };
 	std::string_view myName{ "Waleed" };
 
