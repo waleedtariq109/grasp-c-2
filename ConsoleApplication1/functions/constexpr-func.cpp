65aeda15e36226378b6210facef9c3025066b370
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-25 02:49:43 +0500

    constexpr function

--- ConsoleApplication1/functions/constexpr-func.cpp
+++ ConsoleApplication1/functions/constexpr-func.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
-int main() {
+static constexpr int getValue(int multiplier) {
+	return 3 * multiplier;
+}
 
+int main() {
+	// mark val1 as constexpr if we want to evaluated at compile time
+	constexpr int val1 = getValue(4);
+	std::cout << val1 << std::endl;
+
+	int val2{ 8 };
+	int result = getValue(val2);
+
+	std::cout << result << std::endl;
 }
