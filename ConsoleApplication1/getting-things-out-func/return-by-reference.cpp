dfbac4c1e29bcc2619fe2f42d57a5e13887d92e5
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-28 18:49:38 +0500

    setup

--- ConsoleApplication1/getting-things-out-func/return-by-reference.cpp
+++ ConsoleApplication1/getting-things-out-func/return-by-reference.cpp
@@ -1,23 +1,5 @@
 #include <iostream>
 
-static int& maxNumber(int& a, int& b) {
-	if (a > b) {
-		return a;
-	}
-	else {
-		return b;
-	}
-}
-
 int main() {
-	int val1 = 76;
-	int val2 = 86;
-
-	int& val1Ref = val1;
-	int& val2Ref = val2;
-
-	maxNumber(val1Ref, val2Ref);
-
-	std::cout << "val1Ref: " << val1Ref << std::endl;
-	std::cout << "val2Ref: " << val2Ref << std::endl;
+	
 }
