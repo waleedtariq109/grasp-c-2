d2b8bfd49e55df604e98afa95eec3a1649dc680c
waleedtariq109 <waleedtariq109@outlook.com>
2024-10-30 17:55:49 +0500

    pass by reference

--- ConsoleApplication1/function-template/param-by-reference.cpp
+++ ConsoleApplication1/function-template/param-by-reference.cpp
@@ -3,6 +3,9 @@
 template <typename T>
 T maximumByValue(T param1, T param2);
 
+template <typename T>
+T& maximumByReference(T& param1, T& param2);
+
 int main() {
 	double val1{ 87.99 };
 	double val2{ 76.98 };
@@ -14,6 +17,15 @@ int main() {
 
 	std::cout << "result: " << result << std::endl;
 
+	std::cout << std::endl;
+
+	std::cout << "&val2: " << &val2 << std::endl;
+	auto result1 = maximumByReference(val1, val2);
+
+	std::cout << std::endl;
+
+	std::cout << "result1: " << result1 << std::endl;
+
 }
 
 template <typename T>
@@ -21,6 +33,21 @@ T maximumByValue(T param1, T param2) {
 	/*
 		Receiving the param as value so the changes won't effect the
 		original variable.
+	*/
+	std::cout << "&param1: " << &param1 << std::endl;
+	if (param1 > param2) {
+		return param1;
+	}
+	else {
+		return param2;
+	}
+}
+
+template <typename T>
+T& maximumByReference(T& param1, T& param2) {
+	/*
+	* Receiving the param as reference so any change in this function
+	* template will effect the original variable
 	*/
 	std::cout << "&param1: " << &param1 << std::endl;
 	if (param1 > param2) {
