db6766f19dad54dbfe689fd1f8d0c91544447cc5
waleedtariq109 <waleedtariq109@outlook.com>
2024-11-02 23:14:21 +0500

    type traits

--- ConsoleApplication1/function-template/traits.cpp
+++ ConsoleApplication1/function-template/traits.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <type_traits>
+
+template <typename T>
+void printInteger(T value) {
+	static_assert(std::is_integral<T>::value, "printInteger() only be called with integer aruments");
+	std::cout << "value: " << value << std::endl;
+}
 
 int main() {
+	int a = 8;
+	float b = 87.9f;
+
+	printInteger(a);
 
 }
